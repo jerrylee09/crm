9706d77963b80caeaf50a9149801eb798ab0c97e
jerry lee <jerrylee@jerrys-MacBook-Pro.local>
2020-03-13 15:44:43 -0500

    added people detail and fixed action and reducer

--- src/components/PeopleDetail.js
+++ src/components/PeopleDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ScrollView, Text, Image } from 'react-native';
+import { View, StyleSheet, ScrollView, Text, Image, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 // import EvilIcon from 'react-native-vector-icons/EvilIcons';
 // import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
@@ -10,13 +10,60 @@ import * as actions from '../actions';
 const theme = getTheme();
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: 353,
-        flexWrap: 'wrap',
-        paddingTop: 20,
+    card: {
+        marginTop: 10,
+        paddingBottom: 20,
+        marginBottom: 20,
+        borderColor: 'lightgrey',
+        borderWidth: 0.5,
+    },
+    title1: {
+        top: 10,
+        left: 80,
+        fontSize: 24,
+    },
+    title2: {
+        top: 35,
+        left: 82,
+        fontSize: 18,
+    },
+    image: {
+        flex: 0,
+        height: 100,
+        width: 333,
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    closeIcon: {
+        position: 'absolute',
+        top: 5,
+        left: 295,
+        color: 'rgba(233,166,154,0.8)',
+        backgroundColor: 'rgba(255,255,255,0)',
+    },
+    icon: {
+        position: 'absolute',
+        top: 15,
+        left: 0,
+        color: 'white',
+        backgroundColor: 'rgba(255,255,255,0)',
+    },
+    textArea: {
+        flexDirection: 'row',
         paddingLeft: 20,
-    }
+        paddingTop: 10,
+        width: 260,
+    },
+    textIcons: {
+        color: '#26a69a',
+    },
+    actionArea: {
+        paddingTop: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
 });
 
 class PeopleDetail extends Component {
@@ -29,29 +76,29 @@ class PeopleDetail extends Component {
                         style={[theme.cardImageStyle, styles.image]}
                     />
                     {/*<EvilIcon name={'user'} size={100} style={styles.icon} />*/}
+                    {/*<SimpleIcon name={'close'} size={30} style={styles.closeIcon} onPress={() => this.props.noneSelected()}/>*/}
                     <Text>User</Text>
-                    {/*<SimpleIcon name={'close'} size={30} style={styles.closeIcon}*/}
-                    <Text onPress={() => this.props.noneSelected()} >Close</Text>
+                    <Text onPress={() => this.props.noneSelected()}>Close</Text>
                     <Text style={[theme.cardTitleStyle, styles.title1]}>{this.props.person.firstName} {this.props.person.LastName}</Text>
                     <Text style={[theme.cardTitleStyle, styles.title2]}>from {this.props.person.company}</Text>
                     <View style={styles.textArea}>
-                        {/*<MaterialIcon name={'phone'} size={40} style={styles.textIcons}/>*/}
                         <Text>Phone</Text>
+                        {/*<MaterialIcon name={'phone'} size={40} style={styles.textIcons}/>*/}
                         <Text style={theme.cardContentStyle}>{this.props.person.phone}</Text>
                     </View>
                     <View style={styles.textArea}>
-                        {/*<MaterialIcon name={'email'} size={40} style={styles.textIcons}/>*/}
                         <Text>Email</Text>
+                        {/*<MaterialIcon name={'email'} size={40} style={styles.textIcons}/>*/}
                         <Text style={theme.cardContentStyle}>{this.props.person.email}</Text>
                     </View>
                     <View style={styles.textArea}>
-                        {/*<MaterialIcon name={'assignment'} size={40} style={styles.textIcons}/>*/}
                         <Text>Assignment</Text>
+                        {/*<MaterialIcon name={'assignment'} size={40} style={styles.textIcons}/>*/}
                         <Text style={theme.cardContentStyle}>{this.props.person.project}</Text>
                     </View>
                     <View style={styles.textArea}>
+                        <Text>Edit</Text>
                         {/*<MaterialIcon name={'mode-edit'} size={40} style={styles.textIcons}/>*/}
-                        <Text>Mode Edit</Text>
                         <Text style={theme.cardContentStyle}>{this.props.person.notes}</Text>
                     </View>
                     <View>
@@ -74,10 +121,11 @@ class PeopleDetail extends Component {
                             />
                         </TouchableOpacity>
                         <View style={styles.actionArea}>
-                            <text>Call</text>
-                            <text>Email</text>
-                            <text>SMS</text>
+                            <Text>Call</Text>
+                            <Text>Email</Text>
+                            <Text>SMS</Text>
                         </View>
+                    </View>
                 </ScrollView>
             </View>
         )
@@ -86,9 +134,8 @@ class PeopleDetail extends Component {
 
 const mapStateToProps = state => {
     return {
-        people: state.people,
-        personSelected: state.personSelected,
+        person: state.personSelected,
     }
 }
 
-export default connect(mapStateToProps)(PeopleDetail);
+export default connect(mapStateToProps, actions)(PeopleDetail);
--- src/components/PeopleItem.js
+++ src/components/PeopleItem.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableWithoutFeedback } from 'react-native';
 import { connect } from 'react-redux';
 import { getTheme } from 'react-native-material-kit';
-// import Icon from 'react-native-vector-icons/EvilIcons';
+import Icon from 'react-native-vector-icons/EvilIcons';
 import * as actions from '../actions';
 
 const theme = getTheme();
@@ -34,19 +34,23 @@ const styles = StyleSheet.create({
 
 const PeopleItem = (props) => {
     return (
-        <View style={[theme.cardStyle, styles.card]}>
-            <Image
-                source={require('../images/background.jpg')}
-                style={[theme.cardImageStyle, styles.image]}
-            />
-            {/*<Icon*/}
-            {/*    name={'user'}*/}
-            {/*    size={100}*/}
-            {/*    style={styles.icon}*/}
-            {/*/>*/}
-            <Text style={[theme.cardTitleStyle, styles.title]}>{props.people.firstName} {props.people.lastName}</Text>
-            <Text style={[theme.cardActionStyle, styles.action]}>{props.people.company}</Text>
-        </View>
+        <TouchableWithoutFeedback
+            onPress={() => props.selectPerson(props.people)}>
+            <View style={[theme.cardStyle, styles.card]}>
+                <Image
+                    source={require('../images/background.jpg')}
+                    style={[theme.cardImageStyle, styles.image]}
+                />
+                {/*<Icon*/}
+                {/*    name={'user'}*/}
+                {/*    size={100}*/}
+                {/*    style={styles.icon}*/}
+                {/*/>*/}
+                <Text>User</Text>
+                <Text style={[theme.cardTitleStyle, styles.title]}>{props.people.firstName} {props.people.lastName}</Text>
+                <Text style={[theme.cardActionStyle, styles.action]}>{props.people.company}</Text>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
