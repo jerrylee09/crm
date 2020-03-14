83538b9605ec988421fa2b4e4d4502e4437a95aa
jerry lee <jerrylee@jerrys-MacBook-Pro.local>
2020-03-14 17:44:01 -0500

    added api in action

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,3 +10,38 @@ export const noneSelected = () => {
         type: 'NONE_SELECTED',
     };
 };
+
+
+export const formUpdate = ({ prop, value }) => {
+    return {
+        type: 'FORM_UPDATE',
+        payload: { prop, value },
+    };
+};
+
+export const createNewContact = ({ firstName, lastName, phone, email, company, project, notes }) => {
+    return (dispatch) => {
+        fetch('localhost:3000/contact', {
+        // fetch('http://192.168.2.19:3000/contact', {
+            method: "POST",
+            body: JSON.stringify({
+                "firstName": firstName,
+                "lastName": lastName,
+                "phone": phone,
+                "email": email,
+                "company": company,
+                "project": project,
+                "notes": notes,
+            }),
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+            .then((response) => console.log(response))
+            .then(() => {
+                dispatch({ type: 'NEW_CONTACT' });
+            })
+            .catch(error => console.log(error))
+    };
+};
